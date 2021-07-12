74c2942fe83336c8ea1bd33cb11ceab943e4811a
Isabel <sunfloweer@users.noreply.github.com>
2021-07-12 23:17:16 +0200

    Index button

--- open.js
+++ open.js
@@ -1,6 +1,5 @@
-
-
 document.getElementById("switch").onclick = function () { openLid() };
+
 function openLid() {
     var img = document.getElementById("imageLid");
     img.src = "assets/Opener-Box-Deckel offen.png";
