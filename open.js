2f73ec353996ede72fb827b0410c6f3df5cc2144
vickybremer <58940112+vickybremer@users.noreply.github.com>
2021-07-12 13:20:19 +0200

    box deckel stuff ausprobiert

---
+++ open.js
@@ -0,0 +1,6 @@
+let img = document.querySelector('img');
+let buttonOpen = document.querySelector('#open-box');
+
+buttonOpen.addEventListener('click', () => {
+    img.src = 'assets/Opener-Box-Deckel offen.png'
+});
