0ee6b27f0409c3e168e84abb51bd111f484b1ce3
Isabel <sunfloweer@users.noreply.github.com>
2021-07-13 13:14:41 +0200

    Startscreen fixed
    Bilder positioniert und gefixt, dass kein unterer Rand entsteht.

--- open.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("switch").onclick = function () { openLid() };
-
-function openLid() {
-    var img = document.getElementById("imageLid");
-    img.src = "assets/Opener-Box-Deckel offen.png";
-}
