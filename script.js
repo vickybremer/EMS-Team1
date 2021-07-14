604f896d916149e15d8489381a33ea3273bc3538
vickybremer <58940112+vickybremer@users.noreply.github.com>
2021-07-14 17:18:09 +0200

    TExt ändern auf klick geht hehe

--- script.js
+++ script.js
@@ -9,29 +9,35 @@ let btn7 = document.querySelector('#btn7');
 
 btn1.addEventListener('click', () => {
     img.src = 'assets/inaktiv.png';
-    document.getElementById('unterschrift').innerHTML = "new text";
+    document.getElementById('unterschrift').innerHTML = "THIS IS INAKTIV";
 })
 
 btn2.addEventListener('click', () => {
     img.src = 'assets/neues vokabelpaket.png';
+    document.getElementById('unterschrift').innerHTML = "NEW VOKKIS";
 })
 
 btn3.addEventListener('click', () => {
     img.src = 'assets/offen.png';
+    document.getElementById('unterschrift').innerHTML = "ÖPEN";
 })
 
 btn4.addEventListener('click', () => {
     img.src = 'assets/screen.png';
+    document.getElementById('unterschrift').innerHTML = "SCREAM";
 })
 
 btn5.addEventListener('click', () => {
     img.src = 'assets/light_off.png';
+    document.getElementById('unterschrift').innerHTML = "NO LIGHTS ON IN THIS BRAIN LOL";
 })
 
 btn6.addEventListener('click', () => {
     img.src = 'assets/light_on.png';
+    document.getElementById('unterschrift').innerHTML = "ONE LIGHT ON IN THIS BRAIN";
 })
 
 btn7.addEventListener('click', () => {
     img.src = 'assets/light_wrong.png';
+    document.getElementById('unterschrift').innerHTML = "EE WRONG";
 })
